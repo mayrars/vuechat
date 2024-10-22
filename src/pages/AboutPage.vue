c0151071db1f37ef18f3d49df3cee43cfb9e8a17
mayrars <mayra203@gmail.com>
2024-10-22 07:03:27 -0600

    About page

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <h1>About</h1>
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        :text="['hey, how are you?']"
+        sent
+        name="me"
+      />
+      <q-chat-message
+        :text="['doing fine, how r you?']"
+        name="Luis"
+      />
+    </div>
+  </div>
+  </q-page>
+</template>
