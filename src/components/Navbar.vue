f99f89612b55338c240fe926c00f4a406e74a6fe
mayrars <mayra203@gmail.com>
2024-10-22 07:03:04 -0600

    created navabar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+  <q-header elevated class="bg-primary text-white">
+    <q-toolbar>
+      <q-toolbar-title>
+        Chat
+      </q-toolbar-title>
+
+      <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+    </q-toolbar>
+  </q-header>
+</template>
