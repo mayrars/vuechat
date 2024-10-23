69d2599c2bd66a5fe33c1de1101515250b02be6c
mayrars <mayra203@gmail.com>
2024-10-23 07:05:55 -0600

    created footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+<template>
+  <q-footer elevated class="secondary">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Footer</div>
+        </q-toolbar-title>
+      </q-toolbar>
+  </q-footer>
+</template>
