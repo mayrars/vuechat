e47e76547e3ebc711ffa74839932d858547d2e72
mayrars <mayra203@gmail.com>
2024-10-23 07:05:39 -0600

    Migrated navbar and drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,31 +1,16 @@
 <template>
   <q-layout view="hHr lpR fFr">
-
-
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
+    <Navbar />
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <q-footer elevated class="secondary">
-      <q-toolbar>
-        <q-toolbar-title>
-          <div>Footer</div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
+    <Footer />
 
   </q-layout>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-const rightDrawerOpen = ref(false)
-const toggleRightDrawer = ()=> {
-  rightDrawerOpen.value = !rightDrawerOpen.value
-}
+import Navbar from 'src/components/Navbar.vue';
+import Footer from 'src/components/Footer.vue';
 </script>
