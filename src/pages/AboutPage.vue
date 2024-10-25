99217a1eb85f61c6e7e575cdb6f3c78aa5d5d4eb
mayrars <mayra203@gmail.com>
2024-10-25 07:07:08 -0600

    Delete code

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -2,17 +2,7 @@
   <q-page padding>
     <h1>About</h1>
   <div class="q-pa-md row justify-center">
-    <div style="width: 100%; max-width: 400px">
-      <q-chat-message
-        :text="['hey, how are you?']"
-        sent
-        name="me"
-      />
-      <q-chat-message
-        :text="['doing fine, how r you?']"
-        name="Luis"
-      />
-    </div>
+
   </div>
   </q-page>
 </template>
