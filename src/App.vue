7f97a07046167e719e708c6313f95eca9c0aa160
mayrars <mayra203@gmail.com>
2024-10-25 07:06:16 -0600

    checking if user is logged

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <router-view />
+  <router-view v-if="userGoogle!==false"/>
 </template>
 
 <script setup>
+  import { provide, ref } from 'vue';
   import { getAuth, onAuthStateChanged } from 'firebase/auth';
   import { auth } from './firebase';
-  onAuthStateChanged(auth, (user) => {
-    if(user){
-      const uid = user.uid;
-    }else{
+  import { useQuasar } from 'quasar';
 
-    }
+  const userGoogle = ref(false)
+  const $q = useQuasar()
+
+  provide('userGoogle', userGoogle)
+  onAuthStateChanged(auth, (user) => {
+    userGoogle.value = user
+    $q.loading.hide()
   })
-  if(user)
+
+  $q.loading.show({
+  })
 </script>
