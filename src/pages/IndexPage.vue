b331c78a000bee0bb90ab5c2760338df328f1c20
mayrars <mayra203@gmail.com>
2024-10-25 07:07:26 -0600

    inject user google and create elements for chat

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,24 @@
-<template>
-  <q-page class="flex flex-center">
+<script setup>
+  import { inject } from 'vue';
 
+  const userGoogle = inject("userGoogle")
+</script>
+<template>
+  <q-page v-if="!userGoogle">
+    <h3 class="text-center text-primary">Inicia sesiòn</h3>
+  </q-page>
+  <q-page v-else padding>
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        :text="['hey, how are you?']"
+        sent
+        name="me"
+      />
+      <q-chat-message
+        :text="['doing fine, how r you?']"
+        name="Luis"
+      />
+    </div>
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
-</script>
