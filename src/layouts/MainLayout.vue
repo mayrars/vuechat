03415593145f96da9466100456080e1d47030c74
mayrars <mayra203@gmail.com>
2024-10-25 07:06:58 -0600

    inject user google

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,14 +1,18 @@
 <template>
   <q-layout view="hHr lpR fFr">
-    <Navbar />
+    <navbar />
     <q-page-container>
       <router-view />
     </q-page-container>
-    <Footer />
+    <footer />
   </q-layout>
 </template>
 
 <script setup>
+import {inject} from 'vue'
 import Navbar from 'src/components/Navbar.vue';
 import Footer from 'src/components/Footer.vue';
+
+const userGoogle = inject("userGoogle")
+
 </script>
