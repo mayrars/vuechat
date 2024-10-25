99c9df845cc9c30d7d9eb24ede7ccfe33a8a7a7f
mayrars <mayra203@gmail.com>
2024-10-25 07:06:27 -0600

    created input

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { ref } from 'vue';
+const text = ref('')
 </script>
 <template>
   <q-footer elevated class="secondary">
       <q-toolbar>
-        <q-toolbar-title>
-          <div>Footer</div>
-        </q-toolbar-title>
+        <q-input
+          dark
+          standout="bg-primary text-white"
+          label="Ingrese texto"
+          v-model='text'
+        ></q-input>
       </q-toolbar>
   </q-footer>
 </template>
