3e388c1d395c49dd2e772a2a42ed8f260a67af5e
mayrars <mayra203@gmail.com>
2024-10-26 13:48:52 -0600

    Added button to send and function for save message

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,36 @@
 <script setup>
 import { ref } from 'vue';
+import { collection, addDoc } from 'firebase/firestore';
+import { db, auth } from '../firebase';
+
 const text = ref('')
+const addText=()=>{
+  addDoc(collection(db, 'chats'),{
+    text: text.value,
+    uid: auth.currentUser.uid,
+    time: Date.now(),
+    displayName: auth.currentUser.displayName
+  })
+  .catch(e=>{
+    console.log(e)
+  })
+}
 </script>
 <template>
   <q-footer elevated class="secondary">
       <q-toolbar>
         <q-input
+          class="full-width"
           dark
-          standout="bg-primary text-white"
+          dense
+          standout
           label="Ingrese texto"
           v-model='text'
-        ></q-input>
+        >
+          <template #append>
+            <q-icon name="send" class="cursor-pointer" @click="addText"/>
+          </template>
+        </q-input>
       </q-toolbar>
   </q-footer>
 </template>
